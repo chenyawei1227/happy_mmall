41d146c13e01c9cee589987bcc43180a3e736325
chenyawei1227 <cyw119900@gmail.com>
2018-10-04 17:18:35 +0800

    2018-10-04

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -112,7 +112,7 @@ public class UserController {
     public ServerResponse<User> get_information(HttpSession session){
         User currentUser = (User)session.getAttribute(Const.CURRENT_USER);
         if (currentUser == null){
-            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"未登陆，需要登陆status=10");
+            return ServerResponse.createByErrorMessage("用户未登陆");
         }
         return iUserService.getInformation(currentUser.getId());
     }
---
+++ src/test/java/com/mmall/test/Log4JTest.java
@@ -0,0 +1,26 @@
+package com.mmall.test;
+
+import com.mmall.dao.CartMapper;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+/**
+ * Created by chenyawei on 2017/12/26.
+ */
+public class Log4JTest {
+
+    private final Logger logger = Logger.getLogger(this.getClass());
+    private CartMapper cartMapper;
+    @Test
+    public void test1() {
+        try {
+            cartMapper.insert(null);
+
+        }catch (Exception se){
+            System.out.println(this.getClass().getName() + "common.createTaskInfo");
+            //logger.warn(this.getClass().getName() + "common.createTaskInfo",se);
+            System.out.println(this.getClass().getName()+"成功反馈给推送服务器失败消息：来源sip.js:exports.get_msg_bysip");
+        }
+
+    }
+}
